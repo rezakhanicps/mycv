f855d5ee6bf2e47d197b3102636c060a4852fef8
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-11-10 16:09:58 +0330

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,31 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    //@ts-ignore
+    return this.repo.findOne(id);
+  }
+  find(email: string) {
+    //@ts-ignore
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
